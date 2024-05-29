731f434ba1c9c1c2bbe7470e24d80596e41b49da
mina abiri <mina.abiri@gmail.com>
2024-05-29 16:57:40 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const userData = {
+    name:"mina" , lastName:"abiri" , age:32 , career:"programmer" }
+
+    console.log( "my name is" + " " + userData.name + " "+"and my last name is " + "" + userData.lastName )
